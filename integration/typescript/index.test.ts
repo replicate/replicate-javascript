4d4c548f456c99aadd26e1b1a743b964989dd501
Aron Carroll <aron@replicate.com>
2024-03-05 14:45:50 +0000

    Run `npm run format` on integration files

--- integration/typescript/index.test.ts
+++ integration/typescript/index.test.ts
@@ -1,9 +1,9 @@
-import { test } from 'node:test';
-import assert from 'node:assert';
-import main from './index.js';
+import { test } from "node:test";
+import assert from "node:assert";
+import main from "./index.js";
 
 // Verify exported types.
-import type { 
+import type {
   Status,
   Visibility,
   WebhookEventType,
@@ -18,7 +18,7 @@ import type {
   ServerSentEvent,
 } from "replicate";
 
-test('main', async () => {
+test("main", async () => {
   const output = await main();
   assert.equal(output, "hello Tracy TypeScript");
-});   
+});
