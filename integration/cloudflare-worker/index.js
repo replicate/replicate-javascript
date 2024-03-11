49a49d83d5303e2a3e0ae85fd97dabea8b87d83b
Aron Carroll <aron@replicate.com>
2024-03-11 11:45:49 +0000

    Update CloudFlare worker example to test using streaming LLM

--- integration/cloudflare-worker/index.js
+++ integration/cloudflare-worker/index.js
@@ -1,25 +1,31 @@
 import Replicate from "replicate";
 
-const replicate = new Replicate();
-
 export default {
-  async fetch(_request, _env, _ctx) {
-    const stream = new ReadableStream({
-      async start(controller) {
-        for await (const event of replicate.stream(
-          "replicate/hello-world:5c7d5dc6dd8bf75c1acaa8565735e7986bc5b66206b55cca93cb72c9bf15ccaa",
-          {
-            input: {
-              text: "Colin CloudFlare",
-            },
-          }
-        )) {
-          controller.enqueue(`${event}`);
-        }
-        controller.close();
-      },
-    });
+  async fetch(_request, env, _ctx) {
+    const replicate = new Replicate({ auth: env.REPLICATE_API_TOKEN });
 
-    return new Response(stream);
+    try {
+      const output = replicate.stream(
+        "replicate/canary:30e22229542eb3f79d4f945dacb58d32001b02cc313ae6f54eef27904edf3272",
+        {
+          input: {
+            text: "Colin CloudFlare",
+          },
+        }
+      );
+      const stream = new ReadableStream({
+        async start(controller) {
+          for await (const event of output) {
+            controller.enqueue(new TextEncoder().encode(`${event}`));
+          }
+          controller.enqueue(new TextEncoder().encode("\n"));
+          controller.close();
+        },
+      });
+
+      return new Response(stream);
+    } catch (err) {
+      return Response("", { status: 500 });
+    }
   },
 };
