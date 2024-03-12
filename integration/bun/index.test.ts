38ceef7e87effbc413016610389572e76dcf297f
Mattt Zmuda <mattt@replicate.com>
2024-03-12 06:52:55 -0700

    Add integration test for Bun

---
+++ integration/bun/index.test.ts
@@ -0,0 +1,23 @@
+import { expect, test } from "bun:test";
+import main from "./index.js";
+
+// Verify exported types.
+import type {
+  Status,
+  Visibility,
+  WebhookEventType,
+  ApiError,
+  Collection,
+  Hardware,
+  Model,
+  ModelVersion,
+  Prediction,
+  Training,
+  Page,
+  ServerSentEvent,
+} from "replicate";
+
+test("main", async () => {
+  const output = await main();
+  expect(output as any).toEqual("hello Brünnhilde Bun");
+});
