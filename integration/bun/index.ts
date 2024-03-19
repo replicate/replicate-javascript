f280b0b4c33fe2601d60ac4b2b993488b0358c56
Mattt <mattt@replicate.com>
2024-03-19 01:41:51 -0700

    Switch to replicate.stream in Bun integration test (#227)
    * Switch to replicate.stream in Bun integration test
    
    * Vendor @stardazed/streams-text-encoding
    
    * Modify vendored source of @stardazed/streams-text-encoding
    
    * Use polyfill when TextDecoderStream isn't available
    
    * Use globalThis instead of global
    
    * Set 30s test timeout
    
    * Add information about vendoring and patching TextDecoderStream polyfill
    
    * Use replicate/canary for streaming test
    
    * Fix README

--- integration/bun/index.ts
+++ integration/bun/index.ts
@@ -5,12 +5,20 @@ const replicate = new Replicate({
 });
 
 export default async function main() {
-  return await replicate.run(
-    "replicate/hello-world:5c7d5dc6dd8bf75c1acaa8565735e7986bc5b66206b55cca93cb72c9bf15ccaa",
-    {
-      input: {
-        text: "Brünnhilde Bun",
-      },
+  const model =
+    "replicate/canary:30e22229542eb3f79d4f945dacb58d32001b02cc313ae6f54eef27904edf3272";
+  const options = {
+    input: {
+      text: "Brünnhilde Bun",
+    },
+  };
+  const output = [];
+
+  for await (const { event, data } of replicate.stream(model, options)) {
+    if (event === "output") {
+      output.push(data);
     }
-  );
+  }
+
+  return output.join("").trim();
 }
