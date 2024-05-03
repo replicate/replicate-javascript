ff31e66182a35195f00ec4f24e7b4d8ddd1280ca
Mattt Zmuda <mattt@replicate.com>
2024-05-03 12:58:40 -0700

    Add test coverage for passing signal to replicate.stream

--- integration/cloudflare-worker/index.js
+++ integration/cloudflare-worker/index.js
@@ -5,12 +5,14 @@ export default {
     const replicate = new Replicate({ auth: env.REPLICATE_API_TOKEN });
 
     try {
+      const controller = new AbortController();
       const output = replicate.stream(
         "replicate/canary:30e22229542eb3f79d4f945dacb58d32001b02cc313ae6f54eef27904edf3272",
         {
           input: {
             text: "Colin CloudFlare",
           },
+          signal: controller.signal,
         }
       );
       const stream = new ReadableStream({
